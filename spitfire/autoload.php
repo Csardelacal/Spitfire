e2ef911d0e72d83885f4e1e334624cb896dcff72
cesar <cesar@localhost>
2013-10-30 18:57:33 +0100

    Documenting autoload classes. This is boring as hell.

--- spitfire/autoload.php
+++ spitfire/autoload.php
@@ -23,8 +23,8 @@ class AutoLoad
 	public function makeLocators() {
 		$this->locators[] = new autoload\SystemClassLocator();
 		$this->locators[] = new RegisteredClassLocator();
-		$this->locators[] = new UserClassLocator('Controller', ClassInfo::TYPE_CONTROLLER);
 		$this->locators[] = new autoload\AppClassLocator();
+		$this->locators[] = new UserClassLocator('Controller', ClassInfo::TYPE_CONTROLLER);
 		$this->locators[] = new UserClassLocator('Model',      ClassInfo::TYPE_MODEL);
 		$this->locators[] = new UserClassLocator('Locale',     ClassInfo::TYPE_LOCALE);
 		$this->locators[] = new UserClassLocator('View',       ClassInfo::TYPE_VIEW);
@@ -40,17 +40,37 @@ class AutoLoad
 		foreach ($this->locators as $locator) {
 			if (false !== $file = $locator->getFilenameFor($className)) {
 				include $file;
+				$this->imported_classes[$className] = $file;
 				$this->spitfire->log("Imported class $className...");
 				return true;
 			}
 		}
 		return false;
 	}
+	
+	/**
+	 * 
+	 * @param \spitfire\autoload\ClassLocator $locator
+	 */
+	public function registerLocator($locator) {
+		$this->locators[] = $locator;
+	}
 
 	#######STATIC METHODS#####################
 
 	public static function registerClass($class, $location) {
 		self::$instance->register($class, $location);
+	}
+	
+	/**
+	 * Autoload uses the singleton pattern (you don't have several autoloads in 
+	 * Spitfire) to find classes it needs. Use ClassLocators to add functionality
+	 * to the autoload.
+	 * 
+	 * @return \spitfire\AutoLoad
+	 */
+	public static function getInstance() {
+		return self::$instance;
 	}
 
 }
