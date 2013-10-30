cd43e3d6284ca13fe1057965f94ca71b68c1957b
cesar <cesar@localhost>
2013-10-30 18:57:33 +0100

    Documenting autoload classes. This is boring as hell.

--- spitfire/autoload/classlocator.php
+++ spitfire/autoload/classlocator.php
@@ -1,16 +1,57 @@
 <?php namespace spitfire\autoload;
 
+/**
+ * A class locator is a tool to tell Spitfire where classes are located. This 
+ * usually isn't needed for developers with simple applications, as the base 
+ * locators loaded beforehand by Spitfire are able to locate most of the common
+ * usage classes.
+ * 
+ * But if your app uses special ways of handling file locations for classes you
+ * will need to create your custom Locator and tell Spitfire where the files are
+ * located.
+ * 
+ * @author César de la Cal <cesar@magic3w.com>
+ * @last-revision 2013-10-30
+ */
 abstract class ClassLocator
 {
+	/**
+	 * This method is the main tool of every locator. The implementing class will
+	 * receive a fully qualified name and can either return the filename where 
+	 * it is located in or return false
+	 * 
+	 * @param string|boolean $class The name of the class (fully qualified)
+	 */
 	public abstract function getFilenameFor($class);
 	
-	public function findFile($dir, $name) {
+	/**
+	 * This method will check if there exists a file with the name specified inside
+	 * a directory and if not it will check if the lowercased of the filename
+	 * does exist. 
+	 * 
+	 * @param string $dir
+	 * @param string $name
+	 * @return string|boolean
+	 */
+	public function findFile($dir, $name, $suffix = null) {
 		$file = rtrim($dir, '/') . '/' . $name .'.php';
 		if (file_exists($file)) { return $file; }
 			
 		$file = rtrim($dir, '/') . '/' . strtolower($name) .'.php';
 		if (file_exists($file)) { return $file; }
 		
+		$file = rtrim($dir, '/') . '/' . strtolower($name) . '-' . strtolower($suffix) .'.php';
+		if ($suffix && file_exists($file)) { return $file; }
+		
 		return false;
+	}
+	
+	/**
+	 * Defines this ClassLocator as part of autoload. In oredr to have autoload 
+	 * send classname requests to this file you will need to call this function
+	 * so it is registered.
+	 */
+	public function registerLocator() {
+		\spitfire\AutoLoad::getInstance()->registerLocator($this);
 	}
 }
--- spitfire/autoload/userclasslocator.php
+++ spitfire/autoload/userclasslocator.php
@@ -2,26 +2,91 @@
 
 use Strings;
 
+/**
+ * This locators are meant to find things inside the scope of an application
+ * including Spitfire itself. Classes like controllers, views and models should
+ * be all found by this classes without any issue.
+ * 
+ * I've been trying for quite a while to find a good way to extract the information
+ * about a class from it's name and analyze it inside a class. Something like one
+ * which would provide infromation about the suffix being used and stuff.
+ * 
+ * The problem there is that class locating idoes only happen once in the cycle of
+ * an app. And a class trying to fetch info about another would just make (did make)
+ * the process unnecessarily complicated.
+ * 
+ * @author César de la Cal <cesar@magic3w.com>
+ * @last-revision 2013-10-30
+ */
 class UserClassLocator extends ClassLocator
 {
+	/**
+	 * The class suffix that indicates the type of the class. Like Controller, View
+	 * or Model.
+	 * 
+	 * @var string
+	 */
 	private $suffix;
+	
+	/**
+	 * The type of content the app is looking for. This is important to request 
+	 * from the app that owns the namespace information about the directory where
+	 * it stores said classes.
+	 *
+	 * @var mixed
+	 */
 	private $contenttype;
-
+	
+	/**
+	 * Creates a new class locator for user classes. In Spitfire those are the ones
+	 * that are used by the developers to alter it's behavior, like controllers,
+	 * views and models.
+	 * 
+	 * @param string $suffix
+	 * @param mixed $contenttype
+	 */
 	public function __construct($suffix, $contenttype) {
 		$this->suffix = $suffix;
 		$this->contenttype = $contenttype;
 	}
 	
+	/**
+	 * Gets the filename where a class is located or returns false if the class 
+	 * could not be found. For USerClassLocators this means finding the app for 
+	 * the class it is searching for and then finding it inside the app's directory.
+	 * 
+	 * @param string $class
+	 * @return string|boolean
+	 */
 	public function getFilenameFor($class) {
 		$app = spitfire()->findAppForClass($class);
+		
 		if ($app && Strings::endsWith($class, $this->suffix)) {
-			if (!$this->suffix) {$classURI = explode('\\', substr($class, strlen($app->getNameSpace())));}
-			else { $classURI = explode('\\', substr($class, strlen($app->getNameSpace()), 0 - strlen($this->suffix))); }
+			$classURI = $this->getClassURI($class, $app);
 			$filename = array_pop($classURI);
 			$dir = $app->getDirectory($this->contenttype) . implode(DIRECTORY_SEPARATOR, $classURI);
-			return $this->findFile($dir, $filename);
+			return $this->findFile($dir, $filename, $this->suffix);
 		}
 		return false;
+	}
+	
+	/**
+	 * The class URI is the name of the class without the leading ap inherited
+	 * namespace and without the suffix (in case the class has one). This function
+	 * will return this information already fragmented into an array so it can be
+	 * imploded with the DIRECTORY_SEPARATOR easily
+	 * 
+	 * @param string $class
+	 * @param \App $app
+	 * @return type
+	 */
+	public function getClassURI ($class, $app) {
+		if (!$this->suffix) {
+			return explode('\\', substr($class, strlen($app->getNameSpace())));
+		}
+		else {
+			return explode('\\', substr($class, strlen($app->getNameSpace()), 0 - strlen($this->suffix)));
+		}
 	}
 
 }
