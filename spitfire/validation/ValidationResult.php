3071716d257a6e155d249f6dbc237c380181592d
cesar <cesar@magic3w.com>
2013-11-11 16:17:02 +0100

    Documenting validation classes before moving onto Beans.

--- spitfire/validation/ValidationResult.php
+++ spitfire/validation/ValidationResult.php
@@ -1,14 +1,35 @@
 <?php namespace spitfire\validation;
 
+/**
+ * Objects of this class represent the result of a validation. This means, they
+ * store whether a test was successful or if a 
+ */
 class ValidationResult
 {
-	
+	/**
+	 * The list of errors that the testing of a validator generated. Testing this
+	 * for empty indicates whether a test was successul or not.
+	 * @var ValidationError[]
+	 */
 	private $errors;
 	
+	/**
+	 * Creates a new validation result. This holds information about errors that 
+	 * were found when validating the data that came into your system, usually
+	 * for user data read from POST or GET.
+	 * 
+	 * @param ValidationError[] $errors
+	 */
 	public function __construct($errors = Array()) {
 		$this->errors = array_filter($errors);
 	}
 	
+	/**
+	 * Determines whther the validation of the data was successful. In case it was 
+	 * it will return a boolean true, otherwise a boolean false.
+	 * 
+	 * @return boolean Indicating whether the validation was successful or not
+	 */
 	public function success() {
 		return empty($this->errors);
 	}
--- spitfire/validation/ValidationRule.php
+++ spitfire/validation/ValidationRule.php
@@ -1,6 +1,21 @@
 <?php namespace spitfire\validation;
 
+/**
+ * Classes implementing this one indicate their ability to test a variable and 
+ * verify that it's contents are according to what was expected.
+ * 
+ * @author César de la Cal <cesar@magic3w.com>
+ */
 interface ValidationRule
 {
+	/**
+	 * This method checks if the content of a variable is ok. If this is not so
+	 * it will return a validation error.
+	 * 
+	 * @param mixed $value The value to be tested
+	 * @param mixed $source This value allows to define the origin of an error, 
+	 *         allowing to attach this data next to the source. For example, in forms
+	 *         you can render the errors next to the field.
+	 */
 	function test($value, &$source = null);
 }
