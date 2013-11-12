29b491d93d67b2899429b98c5151998247a4bdfe
cesar <cesar@m3w.atrix>
2013-11-12 16:27:29 +0100

    Documenting validation classes before moving onto Beans.

--- spitfire/validation/Validatable.php
+++ spitfire/validation/Validatable.php
@@ -1,6 +1,19 @@
 <?php namespace spitfire\validation;
 
+/**
+ * Objects implementing this interface must have a validate method that must return
+ * either a ValidationResult object or ValidationError containing the results of 
+ * the validation executed.
+ * 
+ * @author César de la Cal<cesar@magic3w.com>
+ */
 interface Validatable 
 {
+	/**
+	 * Allows the object to test it's contents and see if the data it holds is correct
+	 * and safe for usage or writing into a database.
+	 * 
+	 * @return ValidationResult|ValidationError
+	 */
 	function validate();
 }
