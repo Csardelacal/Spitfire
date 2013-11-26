6f52c8a165f1c26caa669878d765403b08549ab5
cesar <cesar@magic3w.com>
2013-11-26 20:36:16 +0100

    Fixed a bug in MySQL driver which would cause it to derp weirdly when updating before reading. Also added setHeaders to HTMLTable so you don't have to create that row all on your own, planning to add something like that for add row..

--- spitfire/io/html/table.php
+++ spitfire/io/html/table.php
@@ -5,6 +5,13 @@ namespace spitfire\io\html;
 class HTMLTable extends HTMLElement
 {
 	private $rows;
+	
+	public function setHeaders() {
+		$row = new HTMLTableRow();
+		$headers = func_get_args();
+		foreach ($headers as $h) {$row->putCell($h);}
+		$this->putRow($row);
+	}
 
 	public function putRow($row) {
 		$this->rows[] = $row;
