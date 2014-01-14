b35ef64705426a2083b756f556ea4b7b79123438
César de la Cal <flunsidelacal@msn.com>
2014-01-14 20:48:59 +0100

    Finished the date/time adapter

--- spitfire/model/adapters/datetimeadapter.php
+++ spitfire/model/adapters/datetimeadapter.php
@@ -1,10 +1,36 @@
 <?php namespace spitfire\model\adapters;
 
+use DateTime;
+
 /**
  * This class creates an interface between the usual database's string or integer
  * based approach to an object oriented one that allows building apps easier.
  */
 class DateTimeAdapter extends BaseAdapter
 {
-	//TODO: Add time / date handling class
+	/**
+	 * Converts the DateTime Spitfire manages into a Database friendly version 
+	 * (string).
+	 * 
+	 * @return string
+	 */
+	public function dbGetData() {
+		
+		/* @var $datetime DateTime */
+		$datetime = parent::dbGetData();
+		
+		return $datetime->format('Y-m-d H:i:s');
+	}
+	
+	/**
+	 * Takes the database string representing date / time and converts it to a 
+	 * user friendly DateTime object that allows solid object oriented programming
+	 * inside of Spitfire.
+	 * 
+	 * @param DateTime $data
+	 */
+	public function dbSetData($data) {
+		$data = new DateTime(strtotime($data));
+		parent::dbSetData($data);
+	}
 }
