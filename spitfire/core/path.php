df9a4e1d25c73ef7bd141f1d2055afd8c2e96d07
César de la Cal <flunsidelacal@msn.com>
2014-02-20 21:30:24 +0100

    Made some amend and finished documenting Path
    Now I can get over to GET! Yay

--- spitfire/core/path.php
+++ spitfire/core/path.php
@@ -13,6 +13,14 @@
  {
 	 
 	 /**
+	  * The namespace of the app being used. This allows spitfire to locate the 
+	  * application for the namespace and correctly handle it.
+	  * 
+	  * @var string 
+	  */
+	 private $app;
+	 
+	 /**
 	  * The 'name' of the controller expected. The name of the controller is a list
 	  * of string in an array. This is especially useful when mapping a URL
 	  * (<code>/a/url</code>) to a Controller class (<code>\a\urlController</code>)
@@ -62,13 +70,15 @@
 	  * the expected response format and the parameters it took from the URL.
 	  * 
 	  * 
+	  * @param string $app
 	  * @param array  $controller
 	  * @param string $action
 	  * @param array  $object
 	  * @param string $format
 	  * @param array  $parameters
 	  */
-	 public function __construct($controller, $action, $object, $format = 'html', $parameters = Array()) {
+	 public function __construct($app, $controller, $action, $object, $format = 'html', $parameters = Array()) {
+		 $this->app        = $app;
 		 $this->controller = (array)$controller;
 		 $this->action     = $action;
 		 $this->object     = (array)$object;
@@ -76,46 +86,146 @@
 		 $this->parameters = $parameters;
 	 }
 	 
+	 /**
+	  * Returns the namespace of the app chosen to handle the request. This allows
+	  * your application to quickly generate links pointing to the current app.
+	  * 
+	  * @return string
+	  */
+	 public function getApp() {
+		 return $this->app;
+	 }
+	 
+	 /**
+	  * Changes the app in charge of handling the requested Path. Please note that
+	  * this is required to be the namespace of the app, so you will have to retrieve
+	  * this from the app in case you wanna use it.
+	  * 
+	  * @param string $app
+	  * @return \spitfire\core\Path
+	  */
+	 public function setApp($app) {
+		 #If the App is actually an App and the user didn't read the doc, we will
+		 #forgive him.
+		 if ($app instanceof \App) {
+			 $app = $app->getNameSpace();
+		 }
+		 $this->app = $app;
+		 return $this;
+	 }
+	 
+	 /**
+	  * Returns the controller name in array format. This allows you to manipulate
+	  * the fragments easily.
+	  * 
+	  * @return string[]
+	  */
 	 public function getController() {
 		 return $this->controller;
 	 }
-
+	 
+	 /**
+	  * Returns the action the Router parsed from the URL. The action could also
+	  * be a sub-controller, so be aware that this may change during the execution 
+	  * of the request. 
+	  * 
+	  * Usually that's not an issue for the user though.
+	  * 
+	  * @return string
+	  */
 	 public function getAction() {
 		 return $this->action;
 	 }
-
+	 
+	 /**
+	  * Get's the object (the unnamed parameters of the action) that is passed to
+	  * your application via the URL.
+	  * 
+	  * @return string[]
+	  */
 	 public function getObject() {
 		 return $this->object;
 	 }
-
+	 
+	 /**
+	  * Returns the extension appended to the URL and that indicates the type of 
+	  * response the client expects. Your application is free to adequate to this
+	  * or ignore the value.
+	  * 
+	  * @return string
+	  */
 	 public function getFormat() {
 		 return $this->format;
 	 }
-
+	 
+	 /**
+	  * Returns the named parameters that came with the URL. THis are parsed by 
+	  * the router in case it locates named parameter in a user defined route.
+	  * This are especially useful when you want to report data of a url fragment
+	  * that may alter your route (like the language).
+	  * 
+	  * @return string[]
+	  */
 	 public function getParameters() {
 		 return $this->parameters;
 	 }
-
+	 
+	 /**
+	  * Sets the controller name of the class that should handle the request. The
+	  * controller name of \some\randomController would be Array(some, random).
+	  * 
+	  * @param string[] $controller
+	  * @return \spitfire\core\Path
+	  */
 	 public function setController($controller) {
 		 $this->controller = $controller;
 		 return $this;
 	 }
-
+	 
+	 /**
+	  * The name of the action that should be used to respond to the request.
+	  * 
+	  * @param string $action
+	  * @return \spitfire\core\Path
+	  */
 	 public function setAction($action) {
 		 $this->action = $action;
 		 return $this;
 	 }
-
+	 
+	 /**
+	  * Defines the object of the request. The object is an array which holds the
+	  * parameters that will be sent to the action method of the controller.
+	  * 
+	  * @param string $object
+	  * @return \spitfire\core\Path
+	  */
 	 public function setObject($object) {
 		 $this->object = $object;
 		 return $this;
 	 }
-
+	 
+	 /**
+	  * Defines the format which the app should try to respond with. If your app
+	  * is being called directly from a browser this should be something like 
+	  * html, if it's an API you could use JSON, XML or RSS output.
+	  * 
+	  * @param string $format
+	  * @return \spitfire\core\Path
+	  */
 	 public function setFormat($format) {
 		 $this->format = $format;
 		 return $this;
 	 }
-
+	 
+	 /**
+	  * Defines the named parameters that the router (or you) found. These can be
+	  * retrieved by the application by accessing the request object and let you
+	  * define parameters in seemingly random positions of a route.
+	  * 
+	  * @param string $parameters
+	  * @return \spitfire\core\Path
+	  */
 	 public function setParameters($parameters) {
 		 $this->parameters = $parameters;
 		 return $this;
