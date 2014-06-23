a6d7353526dfddcd8fe352edebd17637c635dfe8
César de la Cal <flunsidelacal@msn.com>
2014-06-23 20:37:25 +0200

    Fixed a big that would cause errors in class locators
    If the class locator received a boolean as a parameter it would fail.

--- spitfire/autoload/appclasslocator.php
+++ spitfire/autoload/appclasslocator.php
@@ -5,9 +5,17 @@ use spitfire\ClassInfo;
 
 class AppClassLocator extends ClassLocator
 {
-	
+	/**
+	 * Gets the filename for the expected App. This allows the system to find 
+	 * the required file where the class is contained to be included and used.
+	 *
+	 * @param string $class The complete name of the class
+	 */
 	public function getFilenameFor($class) {
-		
+		if ($class === true || $class === false) {
+			return false;
+		}
+
 		if (Strings::endsWith($class, 'App')) {
 			$classURI = explode('\\', substr($class, 0, 0 - strlen('app')));
 			$filename = 'main';
