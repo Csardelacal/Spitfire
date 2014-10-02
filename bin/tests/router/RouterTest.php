7b0fccf2801a2f7e2bf0faa548b62524f12a44e8
cesar <cesar@cesar-desktop>
2014-10-02 14:32:57 +0200

    Updated broken tests and broken Array rewriting of routes.

--- bin/tests/router/RouterTest.php
+++ bin/tests/router/RouterTest.php
@@ -6,15 +6,15 @@
  * and open the template in the editor.
  */
 
-use \spitfire\core\router\Route;
-use \spitfire\core\router\Router;
+use spitfire\core\router\Route;
+use spitfire\core\router\Router;
 
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
 	
 	public function testCreateRoute() {
 		
-		$router = \spitfire\core\router\Router::getInstance();
+		$router = Router::getInstance();
 		$route  = $router->get('/test', 'test2');
 		
 		$this->assertInstanceOf('\spitfire\core\router\Route', $route);
@@ -26,7 +26,7 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testStringRoute() {
 		
-		$router = Router::getInstance();
+		$router = new Router();
 
 		$route  = $router->get('/test', 'test2');
 		$this->assertEquals(true, $route->test('/test', 'GET', Route::PROTO_HTTP, $router->server()));
@@ -36,6 +36,21 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 		$route2 = $router->get('/another/:param', '/:param/another');
 		$this->assertEquals('/test/another', $route2->rewrite('/another/test', 'GET', Route::PROTO_HTTP, $router->server()));
 		$this->assertEquals(false, $route2->rewrite('/another/test', 'POST', Route::PROTO_HTTP, $router->server()));
+	}
+	
+	public function testArrayRoute() {
+		$router = new Router();
+		
+		$route = $router->get('/:param1/:param2', Array('controller' => 'param1', 'action' => 'param2'));
+		$this->assertEquals(true, $route->test('/another/test', 'GET', Route::PROTO_HTTP, $router->server()));
+		
+		$this->assertInstanceOf('\spitfire\core\Path', $route->rewrite('/another/test', 'GET', Route::PROTO_HTTP, $router->server()));
+		$this->assertInstanceOf('\spitfire\core\Path', $router->server()->rewrite('localhost', '/another/test', 'GET', Route::PROTO_HTTP));
+		
+		$path  = $router->rewrite('localhost', '/another/test', 'GET', Route::PROTO_HTTP);
+		
+		$this->assertEquals('another', $path->getController());
+		$this->assertEquals('test',    $path->getAction());
 	}
 	
 }
