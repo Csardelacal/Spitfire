e7900ccc2aec4ad9d974b654c7d32652c37ca1c2
cesar <cesar@cesar-desktop>
2014-11-04 17:42:58 +0100

    Fixed a bug in the universal name-space based locator. This one will allow the applications to help spitfire locate the required classes for their task more easily.
    Introduced tests the locator.

--- spitfire/autoload/namespacedclasslocator.php
+++ spitfire/autoload/namespacedclasslocator.php
@@ -56,7 +56,7 @@ class NamespacedClassLocator extends ClassLocator
 	 * @param string $suffix
 	 */
 	public function __construct($namespace, $dir, $suffix = '') {
-		$this->namespace = rtrim($namespace,self::NAMESPACE_SEPARATOR) . self::NAMESPACE_SEPARATOR;
+		$this->namespace = trim($namespace,self::NAMESPACE_SEPARATOR) . self::NAMESPACE_SEPARATOR;
 		$this->directory = $dir;
 		$this->suffix    = $suffix;
 	}
@@ -65,13 +65,26 @@ class NamespacedClassLocator extends ClassLocator
 	 * Checks if this class is found inside this namespace. This will check if the 
 	 * namespace is alright and if there is a file that matches the class.
 	 * 
-	 * @param string $class
+	 * @param string $classname
 	 * @return string|boolean
 	 */
-	public function getFilenameFor($class) {
+	public function getFilenameFor($classname) {
+		$class = trim($classname, '\\');
+		#If the namespace and the suffix match we test if the file is found.
 		if (Strings::startsWith($class, $this->namespace) && Strings::endsWith($class, $this->suffix)) {
-			$location = substr($class, strlen($this->namespace), strlen($this->suffix));
-			return $this->findFile($this->directory, $location, $this->suffix);
+			
+			#Get the path WITHIN the namespace that locates the file
+			if (strlen($this->suffix)) {
+				$path = explode(self::NAMESPACE_SEPARATOR, substr($class, strlen($this->namespace), 0 - strlen($this->suffix)));
+			} else {
+				$path = explode(self::NAMESPACE_SEPARATOR, substr($class, strlen($this->namespace)));
+			}
+			
+			#Extract the filename and merge the Path to the namespace with the path within
+			$file = array_pop($path);
+			$directory = array_merge(explode(DIRECTORY_SEPARATOR, $this->directory), $path);
+			#Check for the file
+			return $this->findFile(implode(DIRECTORY_SEPARATOR, $directory), $file, $this->suffix);
 		}
 		
 		return false;
