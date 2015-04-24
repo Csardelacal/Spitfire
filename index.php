1f94384698f7f9ceb13b4587e0016158299db444
César de la Cal <flunsidelacal@msn.com>
2015-04-24 13:29:27 +0200

    Added some more constants

--- index.php
+++ index.php
@@ -12,6 +12,16 @@
  * @copyright 2012 Magic3W - All rights reserved
  */
 
+/* Define bootstrap settings. Environments are a better way to handle
+ * config but we need to create them first.
+ */
+define ('APP_DIRECTORY',         'bin/apps/',        true);
+define ('CONFIG_DIRECTORY',      'bin/settings/',    true);
+define ('CONTROLLERS_DIRECTORY', 'bin/controllers/', true);
+define ('ASSET_DIRECTORY',       'assets/',          true);
+define ('TEMPLATES_DIRECTORY',   'bin/templates/',   true);
+define ('SESSION_SAVE_PATH',     'bin/usr/sessions/',true);
+
 /* Set error handling directives. AS we do not want Apache / PHP
  * to send the data to the user but to our terminal we will tell
  * it to output the errors. Thanks to this linux command:
@@ -22,15 +32,6 @@
 ini_set("log_errors" , "1");
 ini_set("error_log" , "logs/error_log.log");
 ini_set("display_errors" , "0");
-
-/* Define bootstrap settings. Environments are a better way to handle
- * config but we need to create them first.
- */
-define ('APP_DIRECTORY',         'bin/apps/',        true);
-define ('CONFIG_DIRECTORY',      'bin/settings/',    true);
-define ('CONTROLLERS_DIRECTORY', 'bin/controllers/', true);
-define ('ASSET_DIRECTORY',       'assets/',          true);
-define ('TEMPLATES_DIRECTORY',   'bin/templates/',   true);
 
 /* Include Spitfire core.
  */
@@ -48,7 +49,7 @@ include 'spitfire/bootstrap.php';
  */
 $month = 3600*24*30;
 ini_set('session.gc_maxlifetime',$month);
-ini_set('session.save_path', "bin/usr/sessions");
+ini_set('session.save_path', SESSION_SAVE_PATH);
 ini_set('memory_limit', '128M');/**/
 
 /* Call the selected controller with the selected method. */
