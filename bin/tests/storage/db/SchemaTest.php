2d935bb6d084f460aa136ceda665845911edc0ef
cesar <cesar@magic3w.com>
2015-10-03 17:03:51 +0200

    Finishing the changes on the Schema. Need to document the tests and we're good to go.

--- bin/tests/storage/db/SchemaTest.php
+++ bin/tests/storage/db/SchemaTest.php
@@ -61,6 +61,42 @@ class SchemaTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($a, $a->a->getModel());
 	}
 	
+	/**
+	 * Tests whether the Schema allows removal of a field that is no longer needed
+	 * for the usage of the database.
+	 * 
+	 * Unsetting a field is usually only needed when working with very special case
+	 * schemas or when using different PKs than the default _id
+	 */
+	public function testUnsettingField() {
+		$a = new \spitfire\storage\database\Schema('test');
+		unset($a->_id);
+	}
+	
+	/**
+	 * 
+	 * @expectedException \spitfire\exceptions\PrivateException
+	 */
+	public function testUnsettingFieldTwice() {
+		$a = new \spitfire\storage\database\Schema('test');
+		unset($a->_id);
+		unset($a->_id);
+	}
+	
+	/**
+	 * Tests whether the Schema allows removal of a field that is no longer needed
+	 * for the usage of the database.
+	 * 
+	 * Unsetting a field is usually only needed when working with very special case
+	 * schemas or when using different PKs than the default _id
+	 */
+	public function testGettingField() {
+		$a = new \spitfire\storage\database\Schema('test');
+		
+		$this->assertEquals(null, $a->getField('b'), 'There is no field "B" it should fail');
+		$this->assertInstanceOf(Field::class, $a->getField('_id'), 'The id field should be present');
+	}
+	
 	public function testMakePhysicalFields() {
 		$schema = new \spitfire\storage\database\Schema('test');
 		$table  = db()->table($schema);
