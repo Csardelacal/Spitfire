aef90a61fd6398f3557342021a6826d6d1fed264
César de la Cal <cesar@magic3w.com>
2016-01-11 16:56:06 +0100

    Deprecating olde stuffe!

--- spitfire/core/context.php
+++ spitfire/core/context.php
@@ -3,14 +3,18 @@
 use spitfire\core\Request;
 use spitfire\cache\MemcachedAdapter;
 use publicException;
-use privateException;
+use spitfire\exceptions\PrivateException;
 use spitfire\InputSanitizer;
 use spitfire\core\annotations\ActionReflector;
 use spitfire\core\Response;
 
 /**
- * The context is a wrapper for an application that 'does stuff' it basically
- * gathers a basic set of MVC tools and executes them to let things happen.
+ * The context is a wrapper for an Intent. Basically it describes a full request
+ * for a page inside Spitfire. Usually you would have a single Context in any 
+ * execution.
+ * 
+ * Several contexts will usually only be found in Unit Tests that mock the context,
+ * when using nested controllers or in a CLI application.
  * 
  * @link http://www.spitfirephp.com/wiki/index.php/Class:Context
  * @author César de la Cal <cesar@magic3w.com>
@@ -18,9 +22,29 @@ use spitfire\core\Response;
  */
 class Context
 {
+	/**
+	 * This is a reference to the context itself. This is a little helper for the
+	 * View and Controller objects that do expose the Context's elements via Magic
+	 * methods, this way we do not need any extra cases for the context.
+	 *
+	 * @var Context
+	 */
 	public $context;
 	
+	/**
+	 * The application running the current context. The app will provide the controller
+	 * to handle the request / context provided.
+	 *
+	 * @var \App
+	 */
 	public $app;
+	
+	/**
+	 * The controller is in charge of preparig a proper response to the request.
+	 * This is the first logical level that is user-defined.
+	 * 
+	 * @var \Controller
+	 */
 	public $controller;
 	public $action;
 	public $object;
@@ -79,8 +103,8 @@ class Context
 		
 		#Check if the controller can handle the request
 		$request = Array($this->controller, $this->action);
-		if (is_callable($request)) $_return = call_user_func_array($request, $this->object);
-		else throw new publicException('Page not found', 404, new privateException('Action not found', 0));
+		if (is_callable($request)) { $_return = call_user_func_array($request, $this->object); }
+		else { throw new publicException('Page not found', 404, new PrivateException('Action not found', 0)); }
 		
 		if ($_return instanceof Context) { return $_return; }
 		else                             { return $this; }
