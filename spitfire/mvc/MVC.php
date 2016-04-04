538be7860c0d12139e18aacf47d7b16d89e0a51a
cesar <cesar@magic3w.com>
2016-04-04 22:57:06 +0200

    Some minor issues that scrutinizer pointed out together with some doc.

--- spitfire/mvc/MVC.php
+++ spitfire/mvc/MVC.php
@@ -12,6 +12,7 @@ use spitfire\core\Context;
  * the context is made available to inheriting classes.
  * 
  * @property-read View $view The current view
+ * @property-read \App $app The context within this is located
  * @property-read Context $context The context within this is located
  * @property-read core\Request $request The request the context is answering to
  * @property-read \spitfire\core\Response $response Contains the response body and headers
@@ -20,30 +21,38 @@ use spitfire\core\Context;
  * @author César de la Cal <cesar@magic3w.com>
  * 
  */
-
 class MVC extends Pluggable
 {
 	/**
 	 * The context this element belongs to.
 	 *
-	 * @var \spitfire\Context 
+	 * @var Context 
 	 */
 	private $ctx;
 	
-	function __construct(Context$context) {
+	/**
+	 * Create a new MVC base class. This class allows the components that spitfire
+	 * exposes to the developer (controllers and views) to provide a comfortable
+	 * way of accessing some of the more commonly used components of the system
+	 * 
+	 * @param Context $context
+	 */
+	public function __construct(Context$context) {
 		$this->ctx = $context;
 	}
 	
-	function getApp() {
-		return $this->app;
-	}
-	
 	/**
+	 * Provides access to the elements the context shares across the system. This
+	 * allows the app to unify all the features that spitfire offers to the dev
+	 * through a simple interface that is simple to use and understand.
+	 * 
+	 * Using get allows us to expose the contents of the context while making them
+	 * read-only, which is an interesting side effect.
 	 * 
 	 * @param String $variable
 	 * @return controller|view|DBInterface|URL|boolean
 	 */
-	function __get($variable) {
+	public function __get($variable) {
 		if (isset($this->ctx->$variable)) {
 			return $this->ctx->$variable;
 		}
@@ -51,14 +60,19 @@ class MVC extends Pluggable
 	}
 	
 	/**
+	 * This allows the application to report the user that the controller, view and
+	 * other properties this exposes are actually defined. Otherwise the code 
+	 * <code>isset($this->app)</code> would return false, although app is actually
+	 * accessible.
 	 * 
 	 * @param String $name
 	 * @return boolean
 	 */
-	function __isset($name) {
+	public function __isset($name) {
 		switch ($name) {
 			case 'controller':
 			case 'view':
+			case 'app':
 			case 'model':
 			case 'current_url':
 				return true;
