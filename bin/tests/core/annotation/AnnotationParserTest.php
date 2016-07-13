eb75862005bfffe0b76f87408ee0d2dc37339059
César de la Cal <cesar@magic3w.com>
2016-07-13 16:12:45 +0200

    Tested the annotation parser with Reflections too.

--- bin/tests/core/annotation/AnnotationParserTest.php
+++ bin/tests/core/annotation/AnnotationParserTest.php
@@ -1,6 +1,7 @@
 <?php namespace tests\core\annotation;
 
 use PHPUnit_Framework_TestCase;
+use ReflectionClass;
 use spitfire\core\annotations\AnnotationParser;
 
 
@@ -28,4 +29,28 @@ class AnnotationParserTest extends PHPUnit_Framework_TestCase
 		
 	}
 	
+	/**
+	 * 
+	 * @sometest test A
+	 * @sometest test B
+	 */
+	public function testParserReflection() {
+		
+		$parser = new AnnotationParser();
+		$reflec = new ReflectionClass($this);
+		
+		$annotations = $parser->parse($reflec->getMethod('testParserReflection'));
+		
+		#Test the element is actually there
+		$this->assertArrayHasKey('sometest', $annotations);
+		
+		#Ensure it did parse the same annotation twice and properly structure the array
+		$this->assertCount(1, $annotations);
+		$this->assertCount(2, $annotations['sometest']);
+		
+		#Test the value is what we expect
+		$this->assertEquals('test', $annotations['sometest'][0][0]);
+		$this->assertEquals('A',    $annotations['sometest'][0][1]);
+		$this->assertEquals('B',    $annotations['sometest'][1][1]);
+	}
 }
