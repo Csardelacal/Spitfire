90ad46ce8d343f8d931c9385500701fb090d9be6
cesar <cesar@magic3w.com>
2016-07-19 22:41:46 +0200

    Documented the domaingroup object

--- spitfire/locale/DomainGroup.php
+++ spitfire/locale/DomainGroup.php
@@ -26,16 +26,68 @@ use spitfire\exceptions\PrivateException;
  * THE SOFTWARE.
  */
 
+/**
+ * The domain group class allows Spitfire to provide a clean API to the translation
+ * subsystem when using the helper function <code>_t()</code> to manage locales.
+ * 
+ * Since the whole purpose of this class is to provide access to either the default
+ * domain for the application or to the alternative domains it does not contain
+ * barely any logic of it's own.
+ * 
+ * @author César de la Cal Bretschneider <cesar@magic3w.com>
+ */
 class DomainGroup
 {
 	
+	const DEFAULT_DOMAIN = '_default_';
+	
+	/**
+	 * The array of domains registered to be used with this application. A domain
+	 * should uniquely identify your application / component unless it's the final
+	 * user space.
+	 * 
+	 * Recommended is the android / java style naming like com.magic3w.myapp or a
+	 * short name if your application is known uniquely by it.
+	 *
+	 * @var Domain[]
+	 */
 	private $domains;
 	
+	/**
+	 * Gets the default domain for the user space application. This is the namespace
+	 * you should use when you're programming outside an application / module 
+	 * space, since it's the most convenient.
+	 * 
+	 * If you did not register a domain beforehand you will be returned an exception.
+	 * You will have to register a proper Locale for the application first.
+	 * 
+	 * @throws PrivateException If there is no default domain registered
+	 * @return Domain
+	 */
 	public function getDefault() {
-		return $this->domain('_default_');
+		return $this->domain(self::DEFAULT_DOMAIN);
 	}
 	
+	/**
+	 * The domain method provides a comfortable access method to the different
+	 * domains when translating a certain application.
+	 * 
+	 * This method is what allows a user-space application to inject translations
+	 * into prebuilt components to ensure that a user has control over the string
+	 * output of a third party inside his application without editing the vendor's
+	 * source code.
+	 * 
+	 * By using domains we can construct modules which interact with Spitfire apps
+	 * while maintaining their original functionality by providing a proper fallback
+	 * in the event of the framwork not being available.
+	 * 
+	 * @param string $name
+	 * @return Domain
+	 * @throws PrivateException If there is no domain for the given name
+	 */
 	public function domain($name) {
+		
+		#If the domain is not available, we terminate the search. No fallbacks
 		if (!isset($this->domains[$name])) { 
 			throw new PrivateException('Tried localizing without an appropriate locale', 201607191627); 
 		}
@@ -43,21 +95,52 @@ class DomainGroup
 		return $this->domains[$name];
 	}
 	
+	/**
+	 * Adds a domain to the already available list of domains. This method is 
+	 * intended to be provided by the _t() helper function.
+	 * 
+	 * @param string $name
+	 * @param Domain $domain
+	 * @return Domain
+	 */
 	public function putDomain($name, Domain$domain) {
-		if (empty($name)) { $name = '_default_'; }
+		if (empty($name)) { $name = self::DEFAULT_DOMAIN; }
 		$this->domains[$name] = $domain;
 		
 		return $domain;
 	}
 	
+	/**
+	 * Translates a string using the default locale. 
+	 * 
+	 * While this function is definitely functional it's way slower than using _t()
+	 * directly since it has an additional call_user_fun_array() call in it that
+	 * (when PHP7 is ready for widespread adoption) will be replaceable by the 
+	 * splatter operator.
+	 * 
+	 * @throws PrivateException If there is no locale registered
+	 * @return string
+	 */
 	public function say() {
 		return call_user_func_array(Array($this->getDefault(), 'say'), func_get_args());
 	}
 	
+	/**
+	 * Returns the currency object that allows localizing the currency for the 
+	 * default locale.
+	 * 
+	 * @return Currency
+	 */
 	public function currency() {
 		return $this->getDefault()->currency();
 	}
 	
+	/**
+	 * The date formatter object will provide access to date formatting according
+	 * to the default locale.
+	 * 
+	 * @return DateFormatter
+	 */
 	public function date() {
 		return $this->getDefault()->date();
 	}
