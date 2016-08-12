dfac56da350d63be682981e1fd69e22c48931c7d
César de la Cal <flunsidelacal@msn.com>
2016-08-12 11:45:32 +0200

    Removing library specific tests
    These tests do not check whether the application behaves properly when
    working with the framework itself and therefore should only be inside
    the Engine repo.

--- bin/tests/autoload/NamespacedclasslocatorTest.php
+++ bin/tests/autoload/NamespacedclasslocatorTest.php
@@ -7,22 +7,6 @@ class NamespacedclasslocatorTest extends \PHPUnit_Framework_TestCase
 	
 	private $locator;
 	
-	public function testNameSpacedClassLocator() {
-		$this->locator = new NamespacedClassLocator('spitfire', spitfire()->getCWD() . '/spitfire');
-		
-		$this->assertEquals(false, $this->locator->getFilenameFor('\spitfire\somenamespace\someRandomClassThatDoesNotExist'), 
-				'If a class does not exist the test should fail.');
-		
-		$this->assertEquals(false, $this->locator->getFilenameFor('\controllers\MyController'), 
-				'A class locator should not match wrong namespaces.');
-		
-		$this->assertNotEquals(false, $this->locator->getFilenameFor('spitfire\core\router\Router'), 
-				'The class Locator in spitfire should find the router. No leading slash.');
-		
-		$this->assertNotEquals(false, $this->locator->getFilenameFor('\spitfire\core\router\Router'), 
-				'The class Locator in spitfire should find the router.');
-	}
-	
 	public function testLookingForControllers() {
 		$locator = new NamespacedClassLocator('', spitfire()->getCWD() . '/bin/controllers', 'Controller');
 		
