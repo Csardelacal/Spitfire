ca04688d5cc93a4933f0504efce41740b834e1bf
César de la Cal <flunsidelacal@msn.com>
2016-08-12 12:10:57 +0200

    Bumped spitfire
    Also stopped the awful old environment. This should allow to remove the
    deprecated file inside spitfire.

--- bin/settings/environments.php
+++ bin/settings/environments.php
@@ -1,5 +1,10 @@
 <?php
 
-$e = new spitfire\environment('test');
+use spitfire\core\Environment;
 
+/*
+ * Creates a test environment that can be used to store configuration that affects
+ * the behavior of an application.
+ */
+$e = new Environment('test');
 $e->set('db_table_prefix', 'test_');
