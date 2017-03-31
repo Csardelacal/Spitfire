bba871c731269047e2d972996e6ae9e2f9744237
cesar <cesar@magic3w.com>
2017-03-31 16:13:44 +0200

    Fixed the error page using the deprecated environments.

--- bin/error_pages/default.php
+++ bin/error_pages/default.php
@@ -1,7 +1,6 @@
 <?php
 
-use spitfire\SpitFire;
-use spitfire\environment;
+use spitfire\core\Environment;
 
 ?><!DOCTYPE html>
 <html>
@@ -76,7 +75,7 @@ use spitfire\environment;
 			</div>
 		</div>
 
-		<?php if(environment::get('debugging_mode')): ?>
+		<?php if(Environment::get('debugging_mode')): ?>
 		<div class="errordescription wrapper">
 			<h2>Further error information <small>To hide this set debug_mode to false.</small></h2>
 			<p>The stacktrace displays the function calls made that led to the error. They are displayed in an inverted order to how they were called.</p>
