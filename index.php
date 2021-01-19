d7fd357b2f1e065c009791db07da515126019f91
César de la Cal Bretschneider <cesar@magic3w.com>
2021-01-19 10:16:54 +0100

    Updating the project template repo to SF2020
    Test Plan: This version uses composer to bootstrap the system instead of it's own mechanisms
    
    Reviewers: sebastian
    
    Reviewed By: sebastian
    
    Differential Revision: https://phabricator.magic3w.com/D447

--- index.php
+++ index.php
@@ -12,17 +12,6 @@
  * @copyright 2012 Magic3W - All rights reserved
  */
 
-/* Define bootstrap settings. Environments are a better way to handle
- * config but we need to create them first.
- */
-
-define ('BASEDIR',                rtrim(dirname(__FILE__),'\/'), true);
-define ('APP_DIRECTORY',         'bin/apps/',        true);
-define ('CONFIG_DIRECTORY',      'bin/settings/',    true);
-define ('CONTROLLERS_DIRECTORY', 'bin/controllers/', true);
-define ('ASSET_DIRECTORY',       'assets/',          true);
-define ('TEMPLATES_DIRECTORY',   'bin/templates/',   true);
-define ('SESSION_SAVE_PATH',     'bin/usr/sessions/',true);
 
 /* Set error handling directives. AS we do not want Apache / PHP
  * to send the data to the user but to our terminal we will tell
@@ -34,12 +23,14 @@ define ('SESSION_SAVE_PATH',     'bin/usr/sessions/',true);
 ini_set("log_errors" , 1);
 ini_set("error_log" , "logs/error_log.log");
 ini_set("display_errors" , 0);
-
-/* Include Spitfire core.
- */
-include 'spitfire/bootstrap.php';
-
 ini_set('memory_limit', '128M');/**/
+
+/**
+ * Initialize the autoloader. From here on, the application can start
+ * and run without issue.
+ */
+include __DIR__ . '/vendor/autoload.php';
+
 
 /* Call the selected controller with the selected method. */
 spitfire()->fire();
