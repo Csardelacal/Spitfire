e72336ab398dd22a093e0c046b0822d3da5cdedd
cesar <cesar@magic3w.com>
2021-03-23 11:06:37 +0100

    Spitfire no longer autoselects templates
    Summary: Your application now has to decide which to load
    
    Test Plan: N/A
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D629

---
+++ app/templates/default.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Success!</title>
+	</head>
+	<body>
+		<h1>Success!</h1>
+		<p>
+			Spitfire managed to initialize properly and has been able to generate
+			this response page. To change it, edit the template file that gets loaded
+			from your controller and start editing templates however you like.
+		</p>
+	</body>
+</html>
