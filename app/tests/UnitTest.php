ab9dac9f96e8941aa7d61585de8358c585fc75de
cesar <cesar@magic3w.com>
2021-04-20 11:34:40 +0200

    Introducing unit testing base-line
    Test Plan: PHPUnit should run successfully on this now
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D590

---
+++ app/tests/UnitTest.php
@@ -0,0 +1,13 @@
+<?php namespace app\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+	
+	
+	public function testObvious() {
+		$this->assertEquals(true, true);
+	}
+	
+}
