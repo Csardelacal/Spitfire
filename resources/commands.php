025d44064dc65fc6a90e7c3c5a868cd96639e209
cesar <cesar@magic3w.com>
2021-05-14 10:54:30 +0200

    Added command service provider
    Test Plan:
    This is not yet working. Depends on the kernel implementation being
    proper
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D668

--- resources/commands.php
+++ resources/commands.php
@@ -4,3 +4,6 @@
  * Define your command line interface commands in here, when invoking the appropriate
  * command, Spitfire will call the director you requested.
  */
+return function (string $scope) {
+	
+};
