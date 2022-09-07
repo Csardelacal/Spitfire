128899b975461cf5000f2011fa0292cf64dfe605
cesar <cesar@magic3w.com>
2022-09-07 09:43:20 +0200

    Updated dependencies

--- config/app.php
+++ config/app.php
@@ -2,9 +2,14 @@
 
 return [
 	'providers' => [
+		spitfire\core\router\providers\RouterServiceProvider::class,
 		\app\services\router\RouteProvider::class,
+		spitfire\core\http\URLServiceProvider::class,
 		\app\services\LoggingServiceProvider::class,
 		\spitfire\io\session\SessionProvider::class,
-		\spitfire\mvc\providers\DirectorProvider::class
+		\spitfire\mvc\providers\DirectorProvider::class,
+		spitfire\storage\database\support\services\DatabaseServiceProvider::class,
+		spitfire\storage\database\support\services\MigrationServiceProvider::class,
+		spitfire\model\providers\ModelServiceProvider::class
 	]
 ];
